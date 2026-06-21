d818ea932ae749dc796c29abe37eb773db268e47
Vince Hines <vinjh3@gmail.com>
2026-06-21 16:49:41 -0400

    adds property test for get reading ease label method

--- tests/test_label_logic.py
+++ tests/test_label_logic.py
@@ -1,3 +1,4 @@
+from hypothesis import given, strategies as st
 from main import get_reading_ease_label
 
 def test_label_returns_very_easy_for_high_score():
@@ -23,3 +24,11 @@ def test_label_returns_difficult_for_medium_low_score():
 def test_label_returns_very_difficult_for_very_low_score():
     result = get_reading_ease_label(20)
     assert result == "Very Difficult"
+
+@given(st.floats(allow_nan=False, allow_infinity=False))
+def test_label_is_always_one_of_six_valid_labels(score):
+    label = get_reading_ease_label(score)
+    assert label in [
+        "Very Easy","Easy","Standard",
+        "Fairly Difficult", "Difficult", "Very Difficult"
+    ]
